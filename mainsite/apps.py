f33ed4a51180d203fe5c13de297ad2b2dbdf820b
nicoplus <chch1992@gmail.com>
2017-06-27 23:20:45 +0800

    rename mainsite

--- mainsite/apps.py
+++ mainsite/apps.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class MainsitsConfig(AppConfig):
-    name = 'mainsits'
+class MainsiteConfig(AppConfig):
+    name = 'mainsite'
