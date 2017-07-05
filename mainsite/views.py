69c45867ca82ce1c4bfc8980f8d797a2d6bee13c
nicoplus <chch1992@gmail.com>
2017-07-05 21:44:17 +0800

    lots of files modified

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,6 +1,25 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from datetime import datetime
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.template.loader import get_template
+from django.http import HttpResponse
+
+from models import Post
+
 
 # Create your views here.
+
+def homepage(request):
+	posts = Post.objects.all()
+	now = datetime.now()
+	return render(request, 'index.html', locals())
+
+def showpost(request, slug):
+	post = Post.objects.get(slug = slug)
+	if post:
+		return render(request, 'post.html', locals())
+	return redirect('/')
+
+
--- mblog/urls.py
+++ mblog/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from mainsite.views import homepage, showpost
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', homepage),
+    url(r'^post/(\w+)$', showpost)
 ]
